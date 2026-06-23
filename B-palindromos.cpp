b5a617fdcfb89deecfc174444c4e4424307e6659
liatorresq <ltorresq@unjbg.edu.pe>
2026-06-23 17:55:14 -0500

    Ejercicio B: segundo intento

--- B-palindromos.cpp
+++ B-palindromos.cpp
@@ -8,9 +8,11 @@ int main() {
     for (int i = 0; i < N; i++) {
         cin >> palabra;
         bool esPalindromo = true;
-        for (int j = 0; j < palabra.length(); j++) {
-            if (palabra[j] != palabra[palabra.length() - 1 - j]) {
+        int n = palabra.length();
+        for (int j = 0; j < n / 2; j++){
+            if (palabra[j] != palabra[n - 1 - j]) {
                 esPalindromo = false;
+                break;
             }
         }
         if (esPalindromo) {
