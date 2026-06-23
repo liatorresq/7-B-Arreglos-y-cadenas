93e8b8a3d6052091e72c5ba197bba4f582a29d37
liatorresq <ltorresq@unjbg.edu.pe>
2026-06-22 22:15:59 -0500

    Ejercicio A: mejorado

--- A-reverso.cpp
+++ A-reverso.cpp
@@ -4,12 +4,17 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
+
     int arr[1000];
+
     for (int i = 0; i < N; i++) {
         cin >> arr[i];
     }
+
     for (int i = N - 1; i >= 0; i--) {
-        cout << arr[i] << " ";
+        cout << arr[i];
+        if (i > 0) cout << " ";
     }
+
     return 0;
 }
