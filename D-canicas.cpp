16d52d8edf7e6bde8d5b8095b97e46dd3627862c
liatorresq <ltorresq@unjbg.edu.pe>
2026-06-23 23:43:12 -0500

    Ejercicio 4: segundo intento

--- D-canicas.cpp
+++ D-canicas.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main() {
     int N, C;
     cin >> N >> C;
-    int lanzamientos[1000000];
     int vasos[1000001] = {0};
     for (int i = 0; i < C; i++) {
-        cin >> lanzamientos[i];
-    }
-    for (int i = 0; i < C; i++) {
-        int v = lanzamientos[i];
-        vasos[v] = vasos[v] + 1;
+        int v;
+        cin >> v;
+        vasos[v]++;
     }
     for (int i = 1; i <= N; i++) {
         cout << vasos[i] << endl;
