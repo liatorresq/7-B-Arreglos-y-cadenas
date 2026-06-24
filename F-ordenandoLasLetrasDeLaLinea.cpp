27dbe7824489298cccf242493ad9a00b8492e260
liatorresq <ltorresq@unjbg.edu.pe>
2026-06-24 08:14:11 -0500

    Ejercicio 6: segundo intento

--- F-ordenandoLasLetrasDeLaLinea.cpp
+++ F-ordenandoLasLetrasDeLaLinea.cpp
@@ -4,17 +4,29 @@ using namespace std;
 int main() {
     string texto;
     getline(cin, texto);
-    int n = texto.length();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            if (texto[j] >= 'a' && texto[j] <= 'z' &&
-                texto[j+1] >= 'a' && texto[j+1] <= 'z') {
-                if (texto[j] > texto[j+1]) {
-                    char temp = texto[j];
-                    texto[j] = texto[j+1];
-                    texto[j+1] = temp;
-                }
+    char letras[100];
+    int k = 0;
+
+    for (int i = 0; i < texto.length(); i++) {
+         if (texto[i] >= 'a' && texto[i] <= 'z' ) {
+             letras[k] = texto[i];
+            k++;
+         }
+     }  
+    for (int i = 0; i < k; i++){
+        for (int j = 0; j < k -1; j++){
+            if (letras [j] > letras[j + 1]){
+                char temp = letras [j];
+                letras[j] = letras[j + 1];
+                letras [j + 1] = temp;
             }
+        }
+    }
+    int index = 0;
+    for (int i = 0; i < texto.length(); i++){
+        if (texto[i] >= 'a' && texto [i] <= 'z'){
+            texto[i] = letras[index];
+            index++;
         }
     }
     cout << texto << endl;
