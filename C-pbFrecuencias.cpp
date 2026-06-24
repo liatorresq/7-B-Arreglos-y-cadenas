96fb73aa90c856181655b697abc738e4a7464c30
liatorresq <ltorresq@unjbg.edu.pe>
2026-06-23 21:50:43 -0500

    Ejercicio 3: segundo intento

--- C-pbFrecuencias.cpp
+++ C-pbFrecuencias.cpp
@@ -4,14 +4,11 @@ using namespace std;
 int main() {
     int P, n;
     cin >> P >> n;
-    int favoritos[1000];
     int frecuencia[101] = {0};
     for (int i = 0; i < n; i++) {
-        cin >> favoritos[i];
-    }
-    for (int i = 0; i < n; i++) {
-        int f = favoritos[i];
-        frecuencia[f] = frecuencia[f] + 1;
+        int f;
+        cin >> f;
+        frecuencia[f]++;
     }
     for (int i = 1; i <= P; i++) {
         cout << i << "-" << frecuencia[i] << endl;
